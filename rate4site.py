94f43a83286ade5028b9f5a77dac19f0302ae3a2
jacobmarcusambat <jacob.marcus86@gmail.com>
2015-10-04 17:03:09 +0200

    ignore backup files

--- rate4site.py
+++ rate4site.py
@@ -6,9 +6,11 @@ os.chdir('/home/jacob/Desktop/S.AureusCOL_seq/')
 alignment_files = glob.glob('*.aln')
 print alignment_files
 
-rate_out_files = '/home/jacob/Desktop/RESULT/MODULE2'
-if not os.path.exists(rate_out_files):
-	os.makedirs(rate_out_files)
-	with open(os.path.join(rate_out_files,alignment_files)) as rateout:
-		os.system('rate4site -s alignment_files -o rate_out_files')
-rate4site()
+#rate_out_files = '/home/jacob/Desktop/RESULT/MODULE2'
+#if not os.path.exists(rate_out_files):
+	#os.makedirs(rate_out_files)
+	#with open(os.path.join(rate_out_files,alignment_files)) as rateout:
+for aln in alignment_files[:25]:
+	#print aln
+	#os.system('rate4site -s aln -o /home/jacob/Desktop/')
+
